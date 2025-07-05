132343c76d11fd263c50bfbce318cb7b642ef1af
enlazadordm <150435850+enlazadordm@users.noreply.github.com>
2025-07-05 20:36:59 +0200

    Audio basics 2

--- Scripts/Audio/AudioManager.gd
+++ Scripts/Audio/AudioManager.gd
@@ -5,29 +5,38 @@ enum AUDIOBUS {MASTER, MUSIC_M, SFX_M, AMBIENCE_M, MUSIC, UI_SFX, GAME_SFX, AMBI
 enum AUDIOBUS_SCENE {GAME, DIALOGUE, PAUSE}
 
 @export_group("Bus")
-#@export var audiobus_scenes:Array[AudioBusSceneData]
+@export var audiobus_scenes:Array[AudioBusSceneData]
 var music_low_pass_filter_FX:AudioEffectLowPassFilter
+
 @export_group("UI sounds")
 @export var ui_accept_sound:AudioStream
 @export var ui_hover_sound:AudioStream
 @export var ui_close_sound:AudioStream
 
-@export_group("Ciclico sounds")
-@export var hook_sound:AudioStream
-@export var decelerate_loop_sound:AudioStream
-@export var accelerate_loop_sound:AudioStream
-@export var player_hit:AudioStream
+@export_group("Ball sounds")
+@export var roll_sound:AudioStream
+#@export var decelerate_loop_sound:AudioStream
+#@export var accelerate_loop_sound:AudioStream
+#@export var player_hit:AudioStream
+
+@export_group("Ambients")
+@export var bar_background_ambient:AudioStream
+@export var ambient_one_shots:Array[AudioStream]
 
 @export_group("Interactions")
-@export var memory_collected:AudioStream
-@export var flame_gained:AudioStream
-@export var enemy_explosion:AudioStream
+@export var stick_hit_sound:AudioStream
+@export var pool_side_hit_sound:AudioStream
+@export var evil_ball_hit_sound:AudioStream
+@export var evil_ball_missed_sound:AudioStream
+@export var obstacle_hit_sound:AudioStream
+@export var obstacle_destroyed_sound:AudioStream
 
-@onready var intro_song_player:AudioStreamPlayer = %IntroSongPlayer
-@onready var battle_song_player:AudioStreamPlayer = %BattleSongPlayer
+@onready var menu_song_player: AudioStreamPlayer = %MenuSongPlayer
+@onready var game_song_player: AudioStreamPlayer = %GameSongPlayer
 @onready var ambience_player:AudioStreamPlayer = %AmbiencePlayer
 @onready var priority_sfx_audio_player: AudioStreamPlayer = %PrioritySFXAudioPlayer
-@onready var move_shoot_player: AudioStreamPlayer = %MoveShootPlayer
+@onready var roll_sound_player: AudioStreamPlayer = %RollSoundPlayer
+
 
 const MAX_SIMULTANEOUS_SFX := 6
 var sfx_actives := 0
@@ -37,10 +46,10 @@ var previous_audiobus_scene:AUDIOBUS_SCENE
 func _ready() -> void:
 	music_low_pass_filter_FX = AudioServer.get_bus_effect(AUDIOBUS.MUSIC, 0)
 
-func _play_intro_music():
-	intro_song_player.play()
+func _play_menu_music():
+	menu_song_player.play()
 
-func _play_ambience_ia():
+func _play_ambience():
 	ambience_player.play()
 
 func _play_game_sfx_1D(sound:AudioStream, volume_db:float = -6.0, random_scale:float = 1.0, is_priority:bool = false, audio_player:AudioStreamPlayer = priority_sfx_audio_player):
@@ -77,7 +86,7 @@ func _play_ui_sfx(sound:AudioStream, volume_db:float = -6.0) -> void:
 	await player.finished
 	player.queue_free()
 
-func _play_ambience(sound:AudioStream):
+func _play_ambience_one_shot(sound:AudioStream):
 	var player = AudioStreamPlayer.new()
 	add_child(player)
 	player.stream = sound
@@ -86,36 +95,36 @@ func _play_ambience(sound:AudioStream):
 	await player.finished
 	player.queue_free()
 
-func _fade_out_intro_player():
-	var pre_tween_volume = intro_song_player.get_volume_db()
+func _fade_out_menu_player():
+	var pre_tween_volume = menu_song_player.get_volume_db()
 	var fade_out_tween = get_tree().create_tween()
-	fade_out_tween.tween_property(intro_song_player, "volume_db", -48.0, 3.0)
+	fade_out_tween.tween_property(menu_song_player, "volume_db", -48.0, 3.0)
 	await fade_out_tween.finished
-	intro_song_player.stop()
-	intro_song_player.set_volume_db(pre_tween_volume)
+	menu_song_player.stop()
+	menu_song_player.set_volume_db(pre_tween_volume)
 	
-func _fade_out_battle_player(duration:float = 3.0):
-	var pre_tween_volume = battle_song_player.get_volume_db()
+func _fade_out_game_player(duration:float = 3.0):
+	var pre_tween_volume = game_song_player.get_volume_db()
 	var fade_out_tween = get_tree().create_tween()
-	fade_out_tween.tween_property(battle_song_player, "volume_db", -48.0, duration)
+	fade_out_tween.tween_property(game_song_player, "volume_db", -48.0, duration)
 	await fade_out_tween.finished
-	battle_song_player.stop()
-	battle_song_player.set_volume_db(pre_tween_volume)
+	game_song_player.stop()
+	game_song_player.set_volume_db(pre_tween_volume)
 
-func _fade_in_battle_player():
-	var pre_tween_volume = battle_song_player.get_volume_db()
-	battle_song_player.volume_db = -60.0
+func _fade_in_game_player():
+	var pre_tween_volume = game_song_player.get_volume_db()
+	game_song_player.volume_db = -60.0
 	var fade_in_tween = get_tree().create_tween()
-	battle_song_player.play()
-	fade_in_tween.tween_property(battle_song_player, "volume_db", pre_tween_volume, 5.0)
+	game_song_player.play()
+	fade_in_tween.tween_property(game_song_player, "volume_db", pre_tween_volume, 5.0)
 	await fade_in_tween.finished
 
-func _fade_in_intro_player():
-	var pre_tween_volume = intro_song_player.get_volume_db()
-	intro_song_player.volume_db = -60.0
+func _fade_in_menu_player():
+	var pre_tween_volume = menu_song_player.get_volume_db()
+	menu_song_player.volume_db = -60.0
 	var fade_in_tween = get_tree().create_tween()
-	intro_song_player.play()
-	fade_in_tween.tween_property(intro_song_player, "volume_db", pre_tween_volume, 5.0)
+	menu_song_player.play()
+	fade_in_tween.tween_property(menu_song_player, "volume_db", pre_tween_volume, 5.0)
 	await fade_in_tween.finished
 
 func _fade_in_audio_player(audio_player:AudioStreamPlayer, fade_time:float = 3.0):
@@ -142,32 +151,32 @@ func _play_ui_accept_sound():
 func _play_ui_close_sound():
 	_play_ui_sfx(ui_close_sound)
 
-func _play_hook_sound():
-	_play_game_sfx_1D(hook_sound, -3.0, 1.0, true, move_shoot_player)
+func _play_roll_sound():
+	_play_game_sfx_1D(roll_sound, -3.0, 1.0, true, roll_sound_player)
 
-func _play_memory_collected_sound():
-	_play_game_sfx_1D(memory_collected, -10.0)
+#func _play_memory_collected_sound():
+	#_play_game_sfx_1D(memory_collected, -10.0)
+#
+#func _play_decelerate() -> void:
+	#_play_game_sfx_1D(decelerate_loop_sound, -6.0, 1.0, true, move_shoot_player)
 
-func _play_decelerate() -> void:
-	_play_game_sfx_1D(decelerate_loop_sound, -6.0, 1.0, true, move_shoot_player)
+#func _stop_decelerate() -> void:
+	#move_shoot_player.stop()
 
-func _stop_decelerate() -> void:
-	move_shoot_player.stop()
+#func _play_accelerate() -> void:
+	#_play_game_sfx_1D(accelerate_loop_sound, -9.0, 1.0, true, move_shoot_player)
 
-func _play_accelerate() -> void:
-	_play_game_sfx_1D(accelerate_loop_sound, -9.0, 1.0, true, move_shoot_player)
-
-func _stop_accelerate() -> void:
-	move_shoot_player.stop()
+#func _stop_accelerate() -> void:
+	#move_shoot_player.stop()
 	
-func _play_flame_gained(volume_dB:float = -6.0) -> void:
-	_play_game_sfx_1D(flame_gained, volume_dB)
-
-func _play_explosion() -> void:
-	_play_game_sfx_1D(enemy_explosion, 0.0)
-
-func _play_player_hit() -> void:
-	_play_game_sfx_1D(player_hit, 1.0)
+#func _play_flame_gained(volume_dB:float = -6.0) -> void:
+	#_play_game_sfx_1D(flame_gained, volume_dB)
+#
+#func _play_explosion() -> void:
+	#_play_game_sfx_1D(enemy_explosion, 0.0)
+#
+#func _play_player_hit() -> void:
+	#_play_game_sfx_1D(player_hit, 1.0)
 
 func _set_bus_volume(bus:AUDIOBUS, new_db_volume:float) -> void:
 	AudioServer.set_bus_volume_db(bus, new_db_volume)
@@ -187,24 +196,24 @@ func _set_ambience_volume(new_db_volume:float) -> void:
 func _set_music_low_pass_freq(new_freq:float) -> void:
 	music_low_pass_filter_FX.cutoff_hz = new_freq
 
-#func _change_audiobus_scene(new_scene:AUDIOBUS_SCENE, duration:float = 0.1) -> void:
-	#previous_audiobus_scene = current_audiobus_scene
-	#current_audiobus_scene = new_scene
-	#var tween = create_tween().set_parallel()
-	#for bus in audiobus_scenes[new_scene].bus_volumes.keys():
-		#match bus:
-			#AUDIOBUS.MUSIC:
-				#tween.tween_method(_set_music_volume, audiobus_scenes[previous_audiobus_scene].bus_volumes[bus], audiobus_scenes[new_scene].bus_volumes[bus], duration)
-			#AUDIOBUS.UI_SFX:
-				#tween.tween_method(_set_ui_sfx_volume, audiobus_scenes[previous_audiobus_scene].bus_volumes[bus], audiobus_scenes[new_scene].bus_volumes[bus], duration)
-			#AUDIOBUS.GAME_SFX:
-				#tween.tween_method(_set_game_sfx_volume, audiobus_scenes[previous_audiobus_scene].bus_volumes[bus], audiobus_scenes[new_scene].bus_volumes[bus], duration)
-			#AUDIOBUS.AMBIENCE:
-				#tween.tween_method(_set_ambience_volume, audiobus_scenes[previous_audiobus_scene].bus_volumes[bus], audiobus_scenes[new_scene].bus_volumes[bus], duration)
-	#if audiobus_scenes[new_scene].music_low_pass:
-		#tween.tween_method(_set_music_low_pass_freq, audiobus_scenes[previous_audiobus_scene].low_pass_freq, audiobus_scenes[new_scene].low_pass_freq, duration)
-	#else:
-		#tween.tween_method(_set_music_low_pass_freq, audiobus_scenes[previous_audiobus_scene].low_pass_freq, 20000.0, duration)
-#
-#func _revert_previous_audiobus_scene() -> void:
-	#_change_audiobus_scene(previous_audiobus_scene)
+func _change_audiobus_scene(new_scene:AUDIOBUS_SCENE, duration:float = 0.1) -> void:
+	previous_audiobus_scene = current_audiobus_scene
+	current_audiobus_scene = new_scene
+	var tween = create_tween().set_parallel()
+	for bus in audiobus_scenes[new_scene].bus_volumes.keys():
+		match bus:
+			AUDIOBUS.MUSIC:
+				tween.tween_method(_set_music_volume, audiobus_scenes[previous_audiobus_scene].bus_volumes[bus], audiobus_scenes[new_scene].bus_volumes[bus], duration)
+			AUDIOBUS.UI_SFX:
+				tween.tween_method(_set_ui_sfx_volume, audiobus_scenes[previous_audiobus_scene].bus_volumes[bus], audiobus_scenes[new_scene].bus_volumes[bus], duration)
+			AUDIOBUS.GAME_SFX:
+				tween.tween_method(_set_game_sfx_volume, audiobus_scenes[previous_audiobus_scene].bus_volumes[bus], audiobus_scenes[new_scene].bus_volumes[bus], duration)
+			AUDIOBUS.AMBIENCE:
+				tween.tween_method(_set_ambience_volume, audiobus_scenes[previous_audiobus_scene].bus_volumes[bus], audiobus_scenes[new_scene].bus_volumes[bus], duration)
+	if audiobus_scenes[new_scene].music_low_pass:
+		tween.tween_method(_set_music_low_pass_freq, audiobus_scenes[previous_audiobus_scene].low_pass_freq, audiobus_scenes[new_scene].low_pass_freq, duration)
+	else:
+		tween.tween_method(_set_music_low_pass_freq, audiobus_scenes[previous_audiobus_scene].low_pass_freq, 20000.0, duration)
+
+func _revert_previous_audiobus_scene() -> void:
+	_change_audiobus_scene(previous_audiobus_scene)
---
+++ Scripts/Audio/volume_slider.gd
@@ -0,0 +1,25 @@
+extends HSlider
+
+@export var bus:AudioManager.AUDIOBUS
+
+func _init() -> void:
+	focus_entered.connect(_on_focus_entered)
+	mouse_entered.connect(_on_mouse_entered)
+	drag_started.connect(_on_drag_started)
+
+func _on_value_changed(value: float) -> void:
+	AudioManager._set_bus_volume(bus, linear_to_db(value))
+
+func _on_visibility_changed() -> void:
+	if (self.is_visible_in_tree()):
+		value = AudioServer.get_bus_volume_linear(bus)
+
+func _on_focus_entered() -> void:
+	AudioManager._play_ui_hover_sound()
+
+func _on_mouse_entered() -> void:
+	AudioManager._play_ui_hover_sound()
+
+func _on_drag_started() -> void:
+	if bus == AudioManager.AUDIOBUS.SFX_M:
+		AudioManager._play_hook_sound()
