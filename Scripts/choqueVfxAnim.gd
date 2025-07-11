9b509a53d8b791c0acc6659b484eb23823f69a17
Junglepuff <adrianvillarboullosa11@gmail.com>
2025-07-11 20:06:17 +0200

    vfx

---
+++ Scripts/choqueVfxAnim.gd
@@ -0,0 +1,10 @@
+extends Node3D
+
+@onready var chispas = $Chispas
+## @onready var sonido = [añadir el objeto del audio]
+
+func explode():
+	chispas.emitting = true
+## sonido.play()
+	await get_tree().create_timer(2.0).timeout
+	queue_free()
